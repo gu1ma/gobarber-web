ac7ab5738d272cc6659f94d3038c63e0472a5801
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-12-18 00:08:32 -0300

    [Add] Configurando store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+
 import Routes from './routes';
 import history from './services/history';
 
 import './config/ReactotronConfig';
 
+import store from './store';
+
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <GlobalStyle />
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyle />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
