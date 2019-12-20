36f40e601ab6bad5b0271ad86651d23cbb0b0baa
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-12-20 00:16:59 -0300

    [Add] Persistindo autenticacao

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
@@ -8,17 +9,19 @@ import history from './services/history';
 
 import './config/ReactotronConfig';
 
-import store from './store';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <GlobalStyle />
-        <Routes />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <GlobalStyle />
+          <Routes />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
