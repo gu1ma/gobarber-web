d1a0e7e9b7bc644db834b444e07f0e15537b6caa
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-12-29 16:01:35 -0300

    [Add] Navegacao entre dias

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de ' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDays() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDays() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDays}>
           <MdChevronLeft size={36} color="#FFF" />
         </button>
-        <strong>31 de Maio</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDays}>
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
