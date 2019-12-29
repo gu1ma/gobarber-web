eac9d8c142a9ddd8d2a4a2449094e8df4e937eb6
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-12-28 22:41:02 -0300

    [Add] Inicio atualizacao de perfil

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,28 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import {
+  updateProfileFailure,
+  updateProfileSuccess,
+} from '~/store/modules/user/action';
+
+import api from '~/services/api';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+
+    const response = yield call(api.put, 'users', profile);
+
+    yield put(updateProfileSuccess(response.data));
+
+    toast.success('Perfil atualizado com sucesso!');
+  } catch (error) {
+    toast.error('Erro ao atualizar perfil, confira seus dados!');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
